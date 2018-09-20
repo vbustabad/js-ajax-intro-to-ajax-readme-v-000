6e1d26d8d8ca02c70b9c4a85527e4ecaf451f58b
Veronica Bustabad <vbustabad@gmail.com>
2018-09-20 20:12:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+function showRepositories() {
+  console.log(this.responseText);
+  let repoList = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.open('GET', 'https://api.github.com/users/octocat/repos');
